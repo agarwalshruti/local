802e2b3cc91576704f833ea0d735eb4c7abe63ae
hp <hp@Dream>
2018-03-08 11:25:10 +0530

    first commit

---
+++ mavenproject/src/test/java/mavenpackage/mavenproject/DisplayTest.java
@@ -0,0 +1,12 @@
+package mavenpackage.mavenproject;
+
+public class DisplayTest {
+
+	public static void main(String[] args)
+	{
+		System.out.println("good");
+		// TODO Auto-generated method stub
+
+	}
+
+}
