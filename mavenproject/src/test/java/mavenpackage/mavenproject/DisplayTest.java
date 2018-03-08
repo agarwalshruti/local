408d789a1cad8f5f3a455aa80c629f19d72c2b88
agarwalshruti <33657239+agarwalshruti@users.noreply.github.com>
2018-03-08 06:09:48 +0000

    4 th commit code changed

--- mavenproject/src/test/java/mavenpackage/mavenproject/DisplayTest.java
+++ mavenproject/src/test/java/mavenpackage/mavenproject/DisplayTest.java
@@ -7,6 +7,7 @@ public class DisplayTest {
 		System.out.println("good");
 		System.out.println("bad");
 		System.out.println("badddddddd");
+		System.out.println("very badddddddd");
 		// TODO Auto-generated method stub
 
 	}
